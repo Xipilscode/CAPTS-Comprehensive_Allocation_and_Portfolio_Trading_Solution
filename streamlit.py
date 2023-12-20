e439ea252351727da6e96480e31434dbdfa3976a
Alexander Likhachev <120372707+Xipilscode@users.noreply.github.com>
2023-12-19 22:31:26 -0800

    Updated syntax for image header path in  streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -113,7 +113,7 @@ def home():
 def step_1():
     with st.spinner("Loading capital Allocation page..."):
         # Header image 
-        img_header = Image.open('/images/main_page_2.jpeg')
+        img_header = Image.open('images/main_page_2.jpeg')
         st.image(img_header, width=None)
 
         # Header name of Step 1  w/description
